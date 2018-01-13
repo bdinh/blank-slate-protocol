ac62d29087b9a24b77c17beecc36da84bbf8257b
Anirudh Kumar Subramanyam <ask710@uw.edu>
2018-01-13 11:02:02 -0800

    Add firebase configuration and login page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from "firebase";
+
+var config = {
+    apiKey: "AIzaSyCpQ_x0hf9YySPVz_gQPgEEIsHPuw6L-vo",
+    authDomain: "blank-state-protocol.firebaseapp.com",
+    databaseURL: "https://blank-state-protocol.firebaseio.com",
+    projectId: "blank-state-protocol",
+    storageBucket: "blank-state-protocol.appspot.com",
+    messagingSenderId: "79651232490"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
+
+// window.onload = function() {
+//     console.log("javascript loaded");
+//     firebase.auth();
+// }
