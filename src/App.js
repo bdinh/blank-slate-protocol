6d8e8542d4e12f3b34d36ad33588b77b29061acb
bdinh <baodinh96@gmail.com>
2018-01-13 11:35:18 -0800

    Added login view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Landing from './views/landing/landing';
+import LoginView from './views/login/login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Landing/>
+        {/*<Landing/>*/}
+        <LoginView/>
       </div>
     );
   }
