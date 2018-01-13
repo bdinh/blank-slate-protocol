77ee730e521da8bd360a414dc404234da7c2085c
bdinh <baodinh96@gmail.com>
2018-01-13 11:11:59 -0800

    Added landing page for content insertion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import NavBar from './components/navbar';
-import Footer from './components/footer';
+import Landing from './views/landing/landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar/>
-        <Footer/>
+        <Landing/>
       </div>
     );
   }
--- src/views/landing/landing.js
+++ src/views/landing/landing.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import NavBar from './../../components/navbar'
 import Footer from './../../components/footer';
 
-export default class LandingView extends Component {
+export default class Landing extends Component {
     constructor(props) {
         super(props);
     }
@@ -12,6 +12,9 @@ export default class LandingView extends Component {
         return(
             <div>
                 <NavBar/>
+                <div>
+                    Ani fill in content here
+                </div>
                 <Footer/>
             </div>
         )
