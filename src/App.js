67eac63dc94aabbb6c1dac20ce04fbebae436f66
Aaron Nguyen <aaronluannguyen@gmail.com>
2018-01-13 14:22:03 -0800

    My changes to css

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         {/*<Landing/>*/}
-        <LoginView/>
-        {/*<SignUpView/>*/}
+        {/*<LoginView/>*/}
+        <SignUpView/>
       </div>
     );
   }
